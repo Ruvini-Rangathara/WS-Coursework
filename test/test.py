d4be2096c02dbe31369ab50cee7ac8f012751a02
Ruvini Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-09-13 18:16:20 +0530

    test 5

--- test/test.py
+++ test/test.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Add the parent directory to sys.path to find the 'service' and 'model' modules
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 import unittest
 from unittest.mock import MagicMock
 from sqlalchemy.orm import Session
